aa8e3cb49524e678626ed745ea6153f96958ae9f
Tyvnn <ubuntu@ip-172-31-22-211.ec2.internal>
2022-02-28 00:14:41 +0000

    updated providers.tf

--- providers.tf
+++ providers.tf
@@ -8,6 +8,6 @@ terraform {
 
 provider "aws" {
   region                  = "us-east-1"
-  //shared_credentials_file = "C:/Users/Ryan/.aws/credentials"
+  shared_credentials_file = "/home/ubuntu/.aws/credentials"
   //profile                 = "default"
 }
